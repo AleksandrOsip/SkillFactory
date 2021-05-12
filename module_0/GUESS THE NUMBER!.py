67e84252d3b44772f1143c87412c43592c68148d
Александр Осипов <inbhktw72@mail.ru>
2021-05-12 12:43:52 +0500

    12.05.21

--- module_0/GUESS THE NUMBER!.py
+++ module_0/GUESS THE NUMBER!.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def game_core_v2(number):
     '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того,
     больше оно или меньше нужного.
@@ -11,18 +13,20 @@ def game_core_v2(number):
         elif number < predict: # Если число меньше заданного...
             predict -= 1 # ...заданное число уменьшаем на 1.
     return(count) # выход из цикла, если угадали
-print(f"Заданное число {predict}.")
-print(f"Количество попыток {game_core_v2(number)}.")
+    print(f"Заданное число {predict}.")
+    print(f"Количество попыток {game_core_v2(number)}.")
 
+func_1 = game_core_v2(number)
+print(func_1)
 
-def score_game(game_core_v1):
+def score_game(game_core_v2):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
     random_array = np.random.randint(1,101, size=(1000))
     for number in random_array:
-        count_ls.append(game_core_v1(number))
+        count_ls.append(game_core_v2(number))
     score = int(np.mean(count_ls))
     return(score)
 
-print(f"Ваш алгоритм угадывает число в среднем за {score_game(game_core_v1)} попыток")
+print(f"Ваш алгоритм угадывает число в среднем за {score_game(game_core_v2)} попыток")
